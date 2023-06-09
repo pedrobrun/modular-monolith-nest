5251eaac2bf4bcf1a44127d55c95c5a7f5010c51
Pedro Brun <pedroluccavb@gmail.com>
2023-06-08 21:18:00 -0300

    feat: add credit and identity to the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreditModule } from './modules/credit/src/credit.module';
+import { IdentityModule } from './modules/identity/src/identity.module';
 
 @Module({
-  imports: [],
+  imports: [CreditModule, IdentityModule],
   controllers: [AppController],
   providers: [AppService],
 })
