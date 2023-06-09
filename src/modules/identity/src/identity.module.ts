bddf3542c5cd6796c143a0d22060c2b2df0e7903
Pedro Brun <pedroluccavb@gmail.com>
2023-06-08 23:36:32 -0300

    feat(user): add user domain boilerplate

--- src/modules/identity/src/identity.module.ts
+++ src/modules/identity/src/identity.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { IdentityService } from './identity.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   providers: [IdentityService],
 })
 export class IdentityModule {}
