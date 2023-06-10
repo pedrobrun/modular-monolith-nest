696f9bc06dd2b494c2cc38c17252c4f85396730f
Pedro Brun <pedroluccavb@gmail.com>
2023-06-10 12:52:06 -0300

    feat: extract port to env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  Logger.log(`Running on ${PORT}`);
 }
 bootstrap();
