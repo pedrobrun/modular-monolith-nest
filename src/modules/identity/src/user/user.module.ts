e1ad2df83cc87cdd4987f354462aea7f8e618a66
Pedro Brun <pedroluccavb@gmail.com>
2023-06-13 11:08:55 -0300

    feat(gql): format error return

--- src/modules/identity/src/user/user.module.ts
+++ src/modules/identity/src/user/user.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { HttpException, Logger, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserRepository } from './user.repository';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message:
+            (error.extensions?.exception as HttpException)?.message ||
+            error.message,
+        };
+        return graphQLFormattedError;
+      },
     }),
   ],
-  providers: [UserService, UserResolver, UserRepository],
+  providers: [UserService, UserResolver, UserRepository, PrismaService],
 })
 export class UserModule {}
